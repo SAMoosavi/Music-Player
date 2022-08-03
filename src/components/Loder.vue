8a1d9dbf44920bb719a1a81f4b9030db5b0094b9
Moosavi <moosavi238@gmail.com>
2022-08-03 17:36:32 +0430

    new file:   src/components/Loder.vue

---
+++ src/components/Loder.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.0"
+      width="90"
+      height="90"
+      class="fill-transparent stroke-primary"
+    >
+      <path class="stroke-1" ref="element" :d="svgPath" />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { Ref } from "vue";
+import { svgCreat } from "../functions/anime";
+import { loderSvgPath } from "../globalVariable/const";
+
+const svgPath = loderSvgPath;
+const element: Ref<JSX.IntrinsicElements | null> = ref(null);
+onMounted(() => {
+  svgCreat(element.value);
+});
+</script>
