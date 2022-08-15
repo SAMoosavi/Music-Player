40db8e92e8ecb227625e4e83711d2320f65cb932
Moosavi <moosavi238@gmail.com>
2022-08-15 16:19:29 +0430

    add start time and end time  in Player.vue

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -5,14 +5,20 @@
       {{ name }}
       <img width="100" height="100" :src="cover" :alt="`cover-${name}`" class="avatar rounded-full">
       <br>
+      <div class="relative">
+        <div v-if="start" class="absolute bg-red-800 w-1 h-5 -top-3 z-50"
+             :style="`left: ${startTime/duration*100}%`"></div>
+        <div v-if="end" class="absolute bg-green-800 w-1 h-5 -top-3 z-50"
+             :style="`left: ${endTime/duration*100}%`"></div>
+        <input
+            type="range"
+            min="0"
+            :max="duration"
+            :value="time"
+            @input="setTime"
+            class="range range-xs  radio-primary">
+      </div>
 
-      <input
-          type="range"
-          min="0"
-          :max="duration"
-          :value="time"
-          @input="setTime"
-          class="range range-xs radio-primary">
 
       <div class="flex justify-between">
         <span>{{ secondToTime(time / audio.playbackRate) }}</span>
@@ -27,6 +33,11 @@
         <button @click="play(audio)" class="btn btn-secondary basis-1/2 ">play</button>
         <button @click="stop(audio)" class="btn btn-primary basis-1/2 ">stop</button>
       </div>
+      <div class="btn-group ">
+        <button @click="setStart" class="btn btn-secondary basis-1/2 ">setStart</button>
+        <button @click="setEnd" class="btn btn-primary basis-1/2 ">setEnd</button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -41,6 +52,10 @@ import {Music} from "../types/types";
 const props = defineProps<{ music: Music }>()
 const emit = defineEmits(['notFound', "finish"])
 
+const start: Ref<boolean> = ref(false)
+const startTime: Ref<number> = ref(0)
+const end: Ref<boolean> = ref(false)
+const endTime: Ref<number> = ref(0)
 const cover: Ref<string> = ref("")
 const name: Ref<string> = ref("")
 const time: Ref<number> = ref(0)
@@ -74,6 +89,16 @@ if (props.music) {
       duration.value = audio.value.duration
 } else emit('notFound')
 
+function setStart() {
+  startTime.value = audio.value.currentTime
+  start.value = true
+}
+
+function setEnd() {
+  endTime.value = audio.value.currentTime
+  end.value = true
+}
+
 function setPlaybackRate(ev: any) {
   audio.value.playbackRate = ev.target.value
 }
@@ -97,6 +122,11 @@ let getTime: NodeJS.Timer | null = null;
 function play(audio: HTMLAudioElement) {
   audio.play()
   getTime = setInterval(() => {
+    if (start.value && audio.currentTime < startTime.value)
+      audio.currentTime = startTime.value
+    else if (end.value && audio.currentTime > endTime.value)
+      audio.currentTime = startTime.value
+
     time.value = audio.currentTime
     if (time.value == duration.value) {
       if (getTime)
