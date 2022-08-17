139086edf240697c47db4b2484ec9b5be3af4833
Moosavi <moosavi238@gmail.com>
2022-08-17 19:30:11 +0430

    creat palyList/Artists.vue and palyList/Directories.vue

---
+++ src/components/playList/Artists.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @click="setPlayListArtist(0)"
+       class="  btn btn-primary normal-case ">
+    all
+  </div>
+  <div v-for="artist in artistsList " :key="artist" @click="setPlayListArtist(artist)"
+       class="flex justify-between btn btn-primary normal-case">
+    <div class="text-lg">
+      {{ artist }}
+    </div>
+    <div class="text-neutral">
+      [{{ artists[artist].length }}]
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {useMusic} from "../../store/Musics";
+import {storeToRefs} from "pinia";
+import {onMounted, ref, watch} from "vue";
+import type {Ref} from "vue";
+
+const emit = defineEmits(['play'])
+
+const Music = useMusic()
+
+const {artists, allMusic, currentList} = storeToRefs(Music)
+
+const artistsList: Ref<string[]> = ref([])
+
+onMounted(() => {
+  getArtist()
+})
+
+function getArtist() {
+  for (const artistsKey in artists.value) {
+    artistsList.value.push(artistsKey)
+  }
+}
+
+watch(() => artists.value.length, () => getArtist())
+
+function setPlayListArtist(artist: string | 0) {
+  if (artist == 0) {
+    if (!(currentList.value == 'all')) {
+      Music.setCurrentList("all")
+    }
+  } else {
+    if (!(currentList.value == artist)) {
+      Music.setCurrentList(artist, artists.value[artist])
+    }
+  }
+}
+</script>
---
+++ src/components/playList/Directories.vue
@@ -0,0 +1,44 @@
+<template>
+  <div @click="setPlayListDir(0)"
+       class="grid  btn btn-primary normal-case grid-cols-1">
+    all
+  </div>
+  <div v-for="directory in directories " :key="directory" @click="setPlayListDir(directory)"
+       class="grid md:grid-cols-2 btn btn-primary normal-case">
+    <div class="text-lg">
+      {{ directory.split('/').pop() }}
+    </div>
+    <div class="text-xs font-light text-neutral">
+      {{ directory }}
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {useMusic} from "../../store/Musics";
+import {storeToRefs} from "pinia";
+
+const Music = useMusic()
+
+const {directories, allMusic, currentList} = storeToRefs(Music)
+
+function setPlayListDir(dir: string | 0) {
+  if (dir == 0) {
+    if (!(currentList.value == 'all')) {
+      Music.setCurrentList("all")
+    }
+  } else {
+    if (!(currentList.value == dir)) {
+      let list: string[] = []
+      for (const allMusicKey in allMusic.value) {
+        if (allMusicKey.indexOf(dir) == 0) {
+          list.push(allMusicKey)
+        }
+      }
+      Music.setCurrentList(dir, list)
+    }
+  }
+}
+</script>
