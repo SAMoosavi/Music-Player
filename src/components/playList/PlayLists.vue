5b6cdec307343e4ae0dd0edd1bed0991047e4212
Moosavi <moosavi238@gmail.com>
2022-08-18 19:31:48 +0430

    create PlayLists.vue

---
+++ src/components/playList/PlayLists.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-2 ">
+    <div v-for="(_,namePlayList) in playLists" :key="namePlayList" class="btn btn-primary"
+         @click="Music.setCurrentList(namePlayList)">
+      {{ namePlayList }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {useMusic} from "../../store/Musics";
+import {storeToRefs} from "pinia";
+
+const Music = useMusic()
+const {playLists} = storeToRefs(Music)
+</script>
