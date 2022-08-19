da9d4155c8a65d66e79d1ccd8f77f96b09936837
Moosavi <moosavi238@gmail.com>
2022-08-19 17:37:11 +0430

    edit PlayList.vue

--- src/PlayList.vue
+++ src/PlayList.vue
@@ -1,114 +1,1 @@
-<template>
-  <div class="grid md:grid-cols-3">
-    <div class="flex flex-col gap-3 mx-2 mt-5">
-      <div class="btn-group">
-        <button class="btn btn-primary" @click="whichPlayList('dir')">directories</button>
-        <button class="btn btn-secondary" @click="whichPlayList('artist')">artist</button>
-        <button class="btn btn-primary" @click="whichPlayList('addPlayList')">add paly list</button>
-        <button class="btn btn-secondary" @click="whichPlayList('playLists')">play Lists</button>
-      </div>
-      <component :is="playLists[playList]" @play="play" @go-to="goTo"/>
-      <div class="border border-2"></div>
-      <div v-for="a in currentPlayList" :key="a">
-        {{ a }}
-      </div>
-    </div>
-    <Player class="col-span-2" :music="currentMusic" @notFound="" @finish="next" @next="next" @prevent="prevent"
-            :hasNext="hasLoop ||n !== currentPlayList.length - 1" :hasPrevent="hasLoop || n !== 0"
-            @shuffle="shuffle" @loop="loop"
-    />
-  </div>
-
-
-</template>
-
-<script setup lang="ts">
-import Directories from "./components/playList/Directories.vue"
-import Artists from "./components/playList/Artists.vue"
-import AddPlayList from "./components/playList/AddPlayList.vue"
-import PlayLists from "./components/playList/PlayLists.vue"
-import Player from "./components/Player.vue";
-import {onMounted, Ref, ref, watch} from "vue";
-import {storeToRefs} from "pinia";
-import {useMusic} from "./store/Musics";
-
-const Music = useMusic()
-
-const {allMusic, currentPlayList, currentPlayListName} = storeToRefs(Music);
-let key: string = ""
-let n: Ref<number> = ref(0)
-const hasLoop: Ref<boolean> = ref(false)
-
-const playList: Ref<string> = ref("addPlayList")
-
-const playLists = {
-  'dir': Directories,
-  'artist': Artists,
-  'addPlayList': AddPlayList,
-  'playLists': PlayLists
-}
-
-function goTo(name: string) {
-  whichPlayList('playLists')
-}
-
-function whichPlayList(name: string) {
-  playList.value = name
-}
-
-function play() {
-  n.value = 0
-  getCurrentMusic(n.value)
-}
-
-function shuffle() {
-  const shuffledCurrentPlayList = currentPlayList.value.sort(() => 0.5 - Math.random())
-  Music.setCurrentList(Music.currentPlayListName, shuffledCurrentPlayList)
-  n.value = 0;
-  getCurrentMusic(n.value)
-}
-
-function loop() {
-  hasLoop.value = !hasLoop.value
-}
-
-const currentMusic = ref()
-
-
-function prevent() {
-  n.value--;
-  if (n.value < 0) {
-    if (hasLoop.value)
-      n.value = currentPlayList.value.length - 1
-  }
-  getCurrentMusic(n.value)
-}
-
-function next() {
-  n.value++;
-  if (n.value > currentPlayList.value.length - 1) {
-    if (hasLoop.value)
-      n.value = 0
-  }
-  getCurrentMusic(n.value)
-}
-
-function getCurrentMusic(n: number) {
-  key = currentPlayList.value[n]
-  currentMusic.value = allMusic.value[key]
-}
-
-watch(() => currentPlayList.value, () => {
-  n.value = 0
-  getCurrentMusic(n.value)
-})
-
-watch(() => currentPlayListName.value, () => {
-  n.value = 0
-  getCurrentMusic(n.value)
-})
-
-onMounted(() => {
-  getCurrentMusic(n.value)
-})
-</script>
+<template>  <div class="grid md:grid-cols-3">    <div class="flex flex-col gap-3 mx-2 mt-5">      <div class="btn-group">        <button class="btn btn-primary" @click="whichPlayList('dir')">directories</button>        <button class="btn btn-secondary" @click="whichPlayList('artist')">artist</button>        <button class="btn btn-primary" @click="whichPlayList('addPlayList')">add play list</button>        <button class="btn btn-secondary" @click="whichPlayList('playLists')">play Lists</button>      </div>      <component :is="playLists[playList]" @play="play" @go-to="goTo"/>      <div class="border border-2"></div>      <div v-for="a in currentPlayList" :key="a">        {{ a }}      </div>    </div>    <Player class="col-span-2" @notFound="" @finish="next" @next="next" @prevent="prevent"            @shuffle="shuffle" @loop="loop"            :music="currentMusic" :hasLoop="setting.hasLoop" :hasShuffle="setting.hasShuffle"            :hasNext="setting.hasShuffle || setting.hasLoop ||setting.numberOfMusicOnList !== currentPlayList.length - 1"            :hasPrevent="setting.hasShuffle || setting.hasLoop || setting.numberOfMusicOnList !== 0"    />  </div></template><script setup lang="ts">import Directories from "./components/playList/Directories.vue"import Artists from "./components/playList/Artists.vue"import AddPlayList from "./components/playList/AddPlayList.vue"import PlayLists from "./components/playList/PlayLists.vue"import Player from "./components/Player.vue";import {onMounted, ref, watch} from "vue";import type {Ref} from "vue"import {storeToRefs} from "pinia";import {useMusic} from "./store/Musics";import {useSetting} from "./store/setting";const Music = useMusic()const Setting = useSetting()const {allMusic, currentPlayList, currentPlayListName} = storeToRefs(Music);const {setting} = storeToRefs(Setting)const playLists = {  'dir': Directories,  'artist': Artists,  'addPlayList': AddPlayList,  'playLists': PlayLists}const playList: Ref<string> = ref("addPlayList")function goTo(name: string) {  whichPlayList('playLists')}function whichPlayList(name: string) {  playList.value = name}function play() {  Setting.setNumberOfMusicOnList(0)  getCurrentMusic()}function shuffle() {  Setting.setHasShuffle(!setting.value.hasShuffle)}function loop() {  Setting.setHasLoop(!setting.value.hasLoop)}const currentMusic = ref()function getRandom(): number {  let num: number = setting.value.numberOfMusicOnList  while (num == setting.value.numberOfMusicOnList) {    num = Math.floor((Math.random() * currentPlayList.value.length) % currentPlayList.value.length)  }  return num}function prevent() {  if (setting.value.hasShuffle)    Setting.setNumberOfMusicOnList(getRandom())  else    Setting.setNumberOfMusicOnList(setting.value.numberOfMusicOnList - 1);  if (setting.value.numberOfMusicOnList < 0) {    if (setting.value.hasLoop)      Setting.setNumberOfMusicOnList(currentPlayList.value.length - 1)  }  getCurrentMusic()}function next() {  if (setting.value.hasShuffle)    Setting.setNumberOfMusicOnList(getRandom())  else    Setting.setNumberOfMusicOnList(setting.value.numberOfMusicOnList + 1);  if (setting.value.numberOfMusicOnList > currentPlayList.value.length - 1) {    if (setting.value.hasLoop)      Setting.setNumberOfMusicOnList(0)  }  getCurrentMusic()}function getCurrentMusic() {  const n = setting.value.numberOfMusicOnList  console.log(n)  let key = currentPlayList.value[n]  currentMusic.value = allMusic.value[key]}watch(() => currentPlayList.value, () => {  Setting.setNumberOfMusicOnList(0)  getCurrentMusic()})watch(() => currentPlayListName.value, () => {  Setting.setNumberOfMusicOnList(0)  getCurrentMusic()})onMounted(() => {  getCurrentMusic()})</script>
